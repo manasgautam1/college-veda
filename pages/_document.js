d75ef86bd5b6cb3eee81b6423e6e7227a8cdae6c
brandmingo <itsbrandmingo@gmail.com>
2025-05-27 13:12:36 +0530

    google-console-verification-code-added

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+    <meta name="google-site-verification" content="YA2_G8BAGyERe2f3wRM6XsUkiwRHmziYruhfqFly6QI" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
           rel="stylesheet"
