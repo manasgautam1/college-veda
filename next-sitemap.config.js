f6f704e9e0ea6e006f59c4a3ee805ae526f9209e
flyweistTech123 <cseshivangi599@gmail.com>
2025-05-28 15:51:54 +0530

    Add dynamic sitemap and robots.txt using next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://www.collegeveda.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  trailingSlash: false,
+};
